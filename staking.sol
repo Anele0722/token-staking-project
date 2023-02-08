44d1f3f5b67161545af608d668fd1b042a08802a
Anele0722 <114112536+Anele0722@users.noreply.github.com>
2023-02-08 16:45:49 +0200

    Create staking.sol

---
+++ staking.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+ 
+contract staking is ERC20{
+ 
+mapping(address => uint) public staked;
+mapping(address => uint ) private stakedFromTS;
+ 
+constructor() ERC20("Dera Token","DTK"){
+_mint(msg.sender, 10**18);
+}
+ 
+function stake(uint amount) public {
+require(amount >0, "amount <=0");
+require(balanceOf(msg.sender) >= amount, "You do not have up to that amount");
+_transfer(msg.sender, address(this), amount);
+ 
+if(staked[msg.sender] > 0){
+claim();
+}
+ 
+stakedFromTS[msg.sender] = block.timestamp;
+staked[msg.sender] = staked[msg.sender] + amount;
+}
+ 
+function unstake(uint amount) public {
+require(amount >0, "amount <=0");
+ 
+require(staked[msg.sender] > 0, "You did not stake with us");
+_transfer(address(this), msg.sender, amount);
+ 
+stakedFromTS[msg.sender] = block.timestamp;
+staked[msg.sender] = staked[msg.sender] - amount;
+ 
+}
+ 
+}
